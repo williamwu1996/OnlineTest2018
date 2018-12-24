fad6b8b119fd29508ddfe8760e1775c810849cd4
williamwu <732315805@qq.com>
2018-12-24 14:12:44 +0800

    Initial commit

---
+++ src/main/java/com/groupa/OnlinetestApplication.java
@@ -0,0 +1,14 @@
+package com.groupa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinetestApplication.class, args);
+	}
+
+}
+
